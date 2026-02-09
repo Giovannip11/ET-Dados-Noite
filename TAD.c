febe9687776051f4f45d9bde9542ba1eda84b067
Giovannip11 <giggiopinto@gmail.com>
2026-02-09 20:50:28 -0300

    TAD feito

--- TAD.c
+++ TAD.c
@@ -1,35 +1,38 @@
-/*
-Terminar usando TAD
-*/
 #include <stdio.h>
 #include <stdlib.h>
 
 typedef struct {
-    int a;
-    int b;
-}Racional;
+    int a; // numerador
+    int b; // denominador
+} Racional;
 
-
-
-
-Racional cria_racional(int x,int y){
-    if(y==0){
-    printf("zero");
+Racional cria_racional(int x, int y) {
+    if (y == 0) {
+        printf("Erro: denominador zero!\n");
+        exit(1);
     }
-    Racional*racional = (Racional*) malloc(sizeof(*racional));
-    racional->a=x;
-    racional->b=y;
 
-    return *racional;
+    Racional r;
+    r.a = x;
+    r.b = y;
+    return r;
 }
 
-Racional mult(int x, int y){
-    cria_racional(x,y);
-    mult(x,y);
-    mult(x,y);
+Racional mult(Racional x, Racional y) {
+    Racional r;
+    r.a = x.a * y.a;
+    r.b = x.b * y.b;
+    return r;
 }
+
 int main() {
 
+    Racional r1 = cria_racional(2, 3);
+    Racional r2 = cria_racional(4, 5);
+
+    Racional m = mult(r1, r2);
+
+    printf("Resultado: %d/%d\n", m.a, m.b);
 
     return 0;
 }
