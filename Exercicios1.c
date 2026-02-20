943d36c6bc0dc26c29d535030994208904a4a3ce
Giovanni Milan <125603962+Giovannip11@users.noreply.github.com>
2026-02-20 13:48:25 -0300

    Update Exercicios1.c

--- Exercicios1.c
+++ Exercicios1.c
@@ -21,30 +21,34 @@ int main() {
 //2)Vetor dinamico
 #include <stdio.h>
 #include <stdlib.h>
-
 int main() {
-    int n;
-    int TAM=1;
-    int *vetor;
-    int i;
-    
-    while(1){
-        
-        if(n<0){
-            break;
-        }
-         
-        printf("Digite um numero positivo:");
-        scanf("%d",&n);
-        vetor = (int *)malloc(TAM* sizeof(int));   
-        for(i=0;i<=TAM;i++){
-            realloc
-        }
-
-
-      }
-    
-    
+   int *vetor=NULL;
+   int n;
+   int i=0;
+   int j;
+   
+   while(1){
+       printf("Digite um numero para alocar no vetor:");
+       scanf("%d",&n);
+       
+       if(n<0){
+           break;
+       }
+       i++;
+       int *temp = realloc(vetor,i * sizeof(int));
+       if(temp==NULL){
+           printf("ERRO");
+           free(vetor);
+           return 1;
+       }
+       vetor = temp;
+       
+       vetor[i-1]= n;
+    }
+    for (j=0;j<i;j++){
+        printf("%d\t",vetor[j]);
+    }
+    free(vetor);
+   
+   
     return 0;
-}
-
