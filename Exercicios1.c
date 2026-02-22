8f6d759342c699feea968a85c0baede9efab2dd5
Giovanni Milan <125603962+Giovannip11@users.noreply.github.com>
2026-02-22 10:29:57 -0300

    Update Exercicios1.c

--- Exercicios1.c
+++ Exercicios1.c
@@ -52,3 +52,24 @@ int main() {
    
    
     return 0;
+//3)Vetor com swap
+#include <stdio.h>
+#include <stdlib.h>
+void swap_ranges(int *a_begin,int *a_end,int *b_begin){
+    while(a_begin != a_end){
+        
+        int temp = *a_begin;
+        *a_begin = *b_begin;
+        *b_begin = temp;
+        
+        a_begin++;
+        b_begin++;
+    }
+}
+
+int main() {
+    int V[]= {10,20,30,40,50};
+    swap_ranges(&V[0],&V[3],&V[3]);
+  
+    return 0;
+}
