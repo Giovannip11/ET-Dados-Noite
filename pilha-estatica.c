345617434a9bf765f4ec6ffb622648fbbd0ef1dd
Giovanni Milan <125603962+Giovannip11@users.noreply.github.com>
2026-03-04 20:40:13 -0300

    Create pilha-estatica.c

---
+++ pilha-estatica.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+    typedef struct{
+        int info;
+        int T[10];
+    }Pilha;
+
+Pilha inicPilha(void){
+    p->topo = -1;
+}
+void push(Pilha p,int elem){
+    p->topo==p->elem;
+}
+int pop(Pilha p){
+    if()
+    return p;
+}
+int infoPilha(Pilha p,Pilha topo){
+    topo->info;
+    return topo;
+}
+
+int vaziaPilha(Pilha p){
+    if(p!=NULL){
+        return 1;
+    }
+    return -1;
+}
+void destruirPilha(Pilha p){
+    if(topo->info!=NULL){
+        free(Pilha);
+    }
+}
+
+int main() {
+   
+
+    return 0;
+}
